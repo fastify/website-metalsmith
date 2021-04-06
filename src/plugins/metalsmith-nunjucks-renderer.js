38dde3b17ce5af5f34fe42fc743fd6f434524f5d
Arkerone <shaita.axel@gmail.com>
2021-04-06 09:56:00 +0200

    feat: add "multimatch" to process only certain folders or files

--- src/plugins/metalsmith-nunjucks-renderer.js
+++ src/plugins/metalsmith-nunjucks-renderer.js
@@ -2,16 +2,23 @@ const extname = require('path').extname
 const debug = require('debug')('metalsmith-nunjucks-renderer')
 const each = require('async/each')
 const nunjucks = require('nunjucks')
+const multimatch = require('multimatch')
 
 const html = (file) => /\.html$/.test(extname(file))
 
-function plugin () {
+function plugin (opts = {}) {
   debug('Initialized metalsmith-nunjucks')
+  debug('Options: %o', opts)
+
+  const pattern = opts.pattern || ['**/*.html']
 
   return (files, metalsmith, done) => {
     each(
-      Object.keys(files).filter(html),
+      multimatch(Object.keys(files), pattern),
       (filepath, callback) => {
+        if (!html(filepath)) {
+          return callback()
+        }
         debug('Convert html file: %s', filepath)
         const file = files[filepath]
         const metadata = Object.assign({}, file, metalsmith.metadata())
