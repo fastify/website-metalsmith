8c1431d0314f7935ae1e3dd03457f61a7acdf127
Luis <luisorbaiceta@gmail.com>
2021-06-18 15:32:01 +0200

    feat: add hashContent and sanitizePath to utils.js

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,6 +1,8 @@
 const { access } = require('fs')
-const { join } = require('path')
+const { join, basename: getBaseName, dirname: getDirName } = require('path')
 const { mkdir, copyFile, readdir } = require('fs').promises
+const crypto = require('crypto')
+const multimatch = require('multimatch')
 
 function fileExists (path) {
   return new Promise((resolve, reject) => {
@@ -32,7 +34,68 @@ async function copyDir (src, dest) {
   }
 }
 
+function hashContent (options) {
+  // default options
+  options = options || {}
+  if (typeof options.keep !== 'boolean') options.keep = false
+  options.algorithm = options.algorithm || 'sha256'
+  options.pattern = options.pattern || ['**/*.{js,scss,css,map,png,jpg}']
+  options.rename = options.rename || function (filepath, digest) {
+    const basename = getBaseName(filepath)
+    const dirname = getDirName(filepath)
+
+    // we split at the first period, instead of extname
+    //  this is to handle .css.map
+    const ext = basename.indexOf('.')
+
+    return sanitizePath(join(dirname,
+      [
+        basename.substring(0, ext),
+        '.',
+        digest.substr(0, 16),
+        basename.substring(ext)
+      ].join('')))
+  }
+
+  return function (files, ms, done) {
+    const metadata = ms.metadata()
+    metadata.hashes = metadata.hashes || {}
+
+    const relevantFiles = multimatch(Object.keys(files), options.pattern)
+    relevantFiles.forEach(function (filepath) {
+      // this might error, that's Ok.
+      const hash = crypto.createHash(options.algorithm)
+
+      hash.update(files[filepath].contents)
+      const digest = hash.digest('hex')
+
+      const destination = options.rename(filepath, digest)
+
+      files[destination] = files[filepath]
+      if (!options.keep) delete files[filepath]
+
+      filepath = sanitizePath(filepath) // for windows compatibility
+      metadata.hashes[filepath] = destination
+    })
+
+    return process.nextTick(done)
+  }
+}
+
+function sanitizePath (path) {
+  const isExtendedLengthPath = /^\\\\\?\\/.test(path)
+  const hasNonAscii = /[^\u0000-\u0080]+/.test(path) // eslint-disable-line no-control-regex
+
+  if (isExtendedLengthPath || hasNonAscii) {
+    return path
+  }
+
+  return path.replace(/\\/g, '/')
+}
+
 module.exports = {
   copyDir,
-  fileExists
+  fileExists,
+  hashContent,
+  sanitizePath
 }
