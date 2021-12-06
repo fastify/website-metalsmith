a3de24cfed7524743255e436228f8d19cd921c17
lita <luisorbaiceta@gmail.com>
2021-12-06 22:13:02 +0100

    Fix broken Getting Started link

--- src/scripts/processReleases.js
+++ src/scripts/processReleases.js
@@ -18,14 +18,12 @@ needs such.
 
 ## Where To Start
 
-Complete newcomers to Fastify should first read our [Getting Started][gs]
+Complete newcomers to Fastify should first read our [Getting Started](./Getting-Started.md)
 guide.
 
 Developers experienced with Fastify should consult the
 api documentation directly to find the topic they are
 seeking more information about.
-
-[gs]: ./Getting-Started
 `
 
 if (!sourceFolder || !destFolder) {
